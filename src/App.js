c913f53bac83f9d02c4df39d8888c53fd5950a4b
Ahmet Avci <ahmetavci.8873@gmail.com>
2022-10-31 22:42:13 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+
 
 function App() {
+  const[jobs, setJobs] = useState([]);
+  const[loading,setLoading]=useState(true)
+  const[indexValue,setIndexValue]=useState(0)
+
+
+  const fetchJobs=async()=>{
+    
+    const response=await fetch('http://localhost:3001/jobs')
+    const allJobs=await response.json()
+    setJobs(allJobs)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchJobs()
+    },[])
+ 
+
+  if(loading){
+    return(
+      <h1 className="loading">Loading...</h1>
+    )
+  }
+  
+const {duties,title,dates,company}=jobs[indexValue]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <div className="title-section">
+        <h1 className="anabaslik">Experience</h1>
+        <div className="underline"></div>
+      </div>
+      
+      <div className="job-center">
+         <div className="btn-container">
+          {jobs.map((job,index)=>(
+            <button className="btns" key={index} onClick={()=>setIndexValue(index)} >{job.company}</button>
+          ))}
+          
+         </div>
+       
+         <div className="job-info">
+          <h3 className="title">{title}</h3>
+          <h4 className="company">{company}</h4>
+          <p className="date">{dates}</p>
+          {duties.map((duty,index)=>(
+            <p key={index}>{duty}</p>
+          ))}
+         </div>
+      </div>
+    
+    </section>
   );
 }
 
